00f6aad9b3f706008dda739b6059744462be853e
kimjunyoung <kimjy392@gmail.com>
2021-07-28 02:43:07 +0900

    :hammer: fix: 커스텀 useScroll 추가

---
+++ src/components/App/hook.js
@@ -0,0 +1,49 @@
+import api from '../../utils/apis';
+import { useState, useEffect, useRef, useCallback } from 'react';
+
+const useScroll = () => {
+  const [commentData, setCommentData] = useState([]);
+  const [pageIndex, setPageIndex] = useState(1);
+  const observerTarget = useRef(null);
+
+  const getComment = useCallback(async () => {
+    let resData;
+    try {
+        resData = await api.fetchComment(pageIndex, 10);
+    } catch (e) {
+        alert(e)
+        console.log(e)
+    }
+    setCommentData(preData => [...preData, ...resData]);
+    setPageIndex((preState) => preState + 1);
+  }, [pageIndex]);
+
+  useEffect(() => {
+    let observer;
+    console.log(observerTarget.current)
+    if (observerTarget.current){
+        observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    getComment();
+                }
+            });
+        });
+        observer.observe(observerTarget.current);
+    }
+    return () => {
+      console.log(observerTarget.current, 123123)
+      observer && observer.disconnect()
+    };
+  }, [pageIndex])
+
+  useEffect(() => {
+    getComment()
+  }, [])
+
+  
+
+  return [commentData, observerTarget]
+}
+
+export default useScroll
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,65 +1,22 @@
 import './style.css';
-import api from '../../utils/apis';
 import Card from '../Card';
-import { useState, useEffect, useRef, useCallback } from 'react';
-
+import useScroll from './hook';
 
 function App() {
-
-    const { fetchComment } = api;
-    const [commentData, setCommentData] = useState([]);
-    const [pageIndex, setPageIndex] = useState(1);
-
-    const observerTarget = useRef(null);
-    const getComment = useCallback(async () => {
-        let resData;
-        try {
-            resData = await fetchComment(pageIndex, 10);
-        } catch (e) {
-            alert(e)
-            console.log(e)
-        }
-        console.log(resData)
-        setCommentData(preData => [...preData, ...resData]);
-        setPageIndex((preState) => preState + 1);
-    }, [pageIndex]);
-
-
-    useEffect(() => {
-        getComment()
-    }, [])
-
-    useEffect(() => {
-        let observer;
-        console.log(observerTarget.current)
-        if (observerTarget.current){
-            let observer = new IntersectionObserver((entries) => {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        getComment();
-                    }
-                });
-            });
-            observer.observe(observerTarget.current);
-        }
-        return () => observer && observer.disconnect();
-    }, [pageIndex])
-
-    
-
+  const [ commentData, observerTarget ] = useScroll();
   return (
     <div className="App">
       <main className="comments">
         {commentData.map((e, idx) => {
-            return (
-                <Card
-                ref={commentData.length - 1 === idx ? observerTarget : null}
-                key={e.id}
-                commentId={e.id}
-                commentEmail={e.email}
-                commentBody={e.body}
-                />
-            )
+					return (
+						<Card
+						ref={commentData.length - 1 === idx ? observerTarget : null}
+						key={e.id}
+						commentId={e.id}
+						commentEmail={e.email}
+						commentBody={e.body}
+						/>
+					)
         })}
       </main>
     </div>
